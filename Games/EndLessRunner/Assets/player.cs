78be200f189206efc12a2429b7998f14f42a62bf
sunil dhiman <sunil_k_dhiman@outlook.com>
2020-10-14 02:06:09 +0100

    Basic game structure
    1. Added basic game structure
    2. Player moving with button click and press
    3. bouncing
    4. cinemachine following the player

---
+++ Games/EndLessRunner/Assets/player.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private bool leftDir;
+    private bool rightDir;
+    private float HorizontalMove;
+    public float speed = 5;
+    // Jump
+    private bool bounce;
+    public Vector2 jump;
+    public float jumpForce = 2.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start");
+        rb = GetComponent<Rigidbody2D>();
+        leftDir = false;
+        rightDir = false;
+        bounce = false;
+
+        jump = new Vector3(0.0f, 0.2f, 0.0f);
+    }
+
+    public void leftbtnDown()
+    {
+        leftDir = true;
+    }
+    public void leftbtnUp()
+    {
+        leftDir = false;
+    }
+    public void rightbtnDown()
+    {
+        rightDir = true;
+    }
+    public void rightbtnUp()
+    {
+        rightDir = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        movePlayer();
+    }
+    public void Bounce()
+    {
+        bounce = true;
+
+    }
+    private void movePlayer()
+    {
+        if (leftDir)
+        {
+            HorizontalMove = -speed;
+        }
+        else if (rightDir)
+        {
+            HorizontalMove = speed;
+        }
+        else
+        {
+            HorizontalMove = 0;
+        }
+
+    }
+    private void FixedUpdate()
+    {
+
+        rb.velocity = new Vector2(HorizontalMove, rb.velocity.y);
+        if (HorizontalMove == 0 && bounce)
+        {
+            rb.AddForce(new Vector2(0, 10), ForceMode2D.Impulse);
+            bounce = false;
+        }
+    }
+}
