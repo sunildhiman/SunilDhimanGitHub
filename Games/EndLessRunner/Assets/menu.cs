9fbdf92c49a9e0b044cf6462e2251b11c97a9f22
sunil dhiman <sunil_k_dhiman@outlook.com>
2020-10-22 01:10:45 +0100

    Added Menu
    1. added pause button
    2. added quit button
    3. added reset button but not reset function

---
+++ Games/EndLessRunner/Assets/menu.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menu : MonoBehaviour
+{
+    bool gamePaused = false;
+    public GameObject Quitbtn;
+    public GameObject Resetbtn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Quitbtn.SetActive(gamePaused);
+        Resetbtn.SetActive(gamePaused);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void btnpress()
+    {
+        if (gamePaused)
+        {
+            resumeGame();
+        }
+        else
+        {
+            PauseGame();
+        }
+    }
+    private void PauseGame()
+    {
+        Debug.Log("PauseGame");
+        Time.timeScale = 0f;
+        gamePaused = true;
+        Quitbtn.SetActive(true);
+        Resetbtn.SetActive(true);
+    }
+    private void resumeGame()
+    {
+        Debug.Log("PauseGame");
+        Time.timeScale = 1f;
+        gamePaused = false;
+        Quitbtn.SetActive(false);
+        Resetbtn.SetActive(false);
+    }
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+    public void reset()
+    {
+        Debug.Log("PauseGame");
+    }
+}
